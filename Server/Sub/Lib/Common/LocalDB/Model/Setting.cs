7a713a041245e29594a6b34e7b2160bea1527abe
ery <ldh668@gmail.com>
2016-01-27 01:45:28 +0900

    LocalDB 에 누락된 Model 2개를 추가하였음.

---
+++ Server/Sub/Lib/Common/LocalDB/Model/Setting.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+
+namespace IMS.Server.Sub.Lib.LocalDB.Model
+{
+    public class Setting
+    {
+        public int Idx { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public int Code { get; set; }
+        public string Type { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Setting()
+        {
+            
+        }
+
+        public void Set(string key, object value)
+        {
+            Key = key;
+            if (value is int)
+            {
+                Value = ((int) value).ToString();
+                Code = 0;
+                Type = "Integer";
+                UpdatedAt = DateTime.Now;
+            }
+            else if (value is float)
+            {
+                Value = ((float)value).ToString();
+                Code = 1;
+                Type = "Float";
+                UpdatedAt = DateTime.Now;
+            }
+            else if (value is double)
+            {
+                Value = ((double)value).ToString();
+                Code = 2;
+                Type = "Double";
+                UpdatedAt = DateTime.Now;
+            }
+            else if (value is string)
+            {
+                Value = (string) value;
+                Code = 3;
+                Type = "String";
+                UpdatedAt = DateTime.Now;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int GetInt()
+        {
+            int result;
+            if (int.TryParse(Value, out result))
+                return result;
+            return int.MinValue;
+        }
+
+        public float GetFloat()
+        {
+            float result;
+            if (float.TryParse(Value, out result))
+                return result;
+            return float.MinValue;
+        }
+
+        public double GetDouble()
+        {
+            double result;
+            if (double.TryParse(Value, out result))
+                return result;
+            return double.MinValue;
+        }
+
+        public string GetString()
+        {
+            return Value;
+        }
+    }
+
+    public class SettingConfiguration : EntityTypeConfiguration<Setting>
+    {
+        public SettingConfiguration()
+        {
+            ToTable("Setting");
+            HasKey(s => s.Idx);
+            Property(s => s.Idx)
+                .IsRequired()
+                .HasColumnOrder(0)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(s => s.Key)
+                .IsRequired()
+                .HasColumnAnnotation("Idx_Key", new IndexAnnotation(new IndexAttribute()))
+                .HasColumnOrder(1);
+            Property(s => s.Value)
+                .IsOptional()
+                .HasColumnOrder(2);
+            Property(s => s.Code)
+                .IsRequired()
+                .HasColumnOrder(3);
+            Property(s => s.Type)
+                .IsOptional()
+                .HasColumnOrder(4);
+            Property(s => s.UpdatedAt)
+                .IsRequired()
+                .HasColumnOrder(5);
+        }
+    }
+}
---
+++ Server/Sub/Lib/Common/LocalDB/Model/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Text;
+
+namespace IMS.Server.Sub.Lib.LocalDB.Model
+{
+    public class User
+    {
+        public int Idx { get; set; }
+        public string Id { get; set; }
+        public string Passwd { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public bool Authenticate(string id, string passwd)
+        {
+            if (Id != id)
+                return false;
+            var hash = SHA256.Create().ComputeHash(Encoding.Unicode.GetBytes(passwd));
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+            var hashedPasswd = sb.ToString();
+            return Passwd == hashedPasswd;
+        }
+
+        public void CreateUser(string id, string passwd, bool isAdmin = false)
+        {
+            Id = id;
+            var hash = SHA256.Create().ComputeHash(Encoding.Unicode.GetBytes(passwd));
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+            Passwd = sb.ToString();
+            IsAdmin = isAdmin;
+        }
+    }
+
+    public class UserConfiguration : EntityTypeConfiguration<User>
+    {
+        public UserConfiguration()
+        {
+            ToTable("User");
+            HasKey(u => u.Id);
+            Property(u => u.Idx)
+                .IsRequired()
+                .HasColumnOrder(0)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(u => u.Id)
+                .IsRequired()
+                .HasColumnAnnotation("Idx_Id", new IndexAnnotation(new IndexAttribute()))
+                .HasColumnOrder(1);
+            Property(s => s.Passwd)
+                .IsRequired()
+                .HasColumnOrder(2);
+            Property(s => s.IsAdmin)
+                .IsRequired()
+                .HasColumnOrder(3);
+        }
+    }
+}
