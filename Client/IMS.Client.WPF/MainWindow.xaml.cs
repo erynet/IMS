00502b9d3733d89ce78b4fb707d0054470c280d9
Seung Ho Jang <shjang1992@gmail.com>
2016-02-03 21:11:25 +0900

    팝업 관리

--- Client/IMS.Client.WPF/MainWindow.xaml.cs
+++ Client/IMS.Client.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Threading;
 
@@ -13,11 +14,13 @@ namespace IMS.Client.WPF {
         MapPage map = new MapPage();
         SettingPage setting = new SettingPage();
 
+        private List<Window> popupList = new List<Window>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            var popup = new UPSInfoPopup();
+            var popup = CreatePopup<UPSInfoPopup>();
             popup.Show();
 
             Update(null, null);
@@ -28,11 +31,28 @@ namespace IMS.Client.WPF {
             timer.Start();
 
             MainFrame.Navigate(map);
+
+            Closing += OnClose;
         }
 
         private void Update(object sender, EventArgs e)
         {
             Title = "ETI IMS - version 0.1 - " + String.Format(@"{0:yyyy년 MM월 dd일 - HH시 mm분 ss초}", DateTime.Now);
+        }
+
+        private void OnClose(object sender, EventArgs e)
+        {
+            foreach (var popup in popupList) {
+                popup.Close();
+            }
+        }
+
+        public Window CreatePopup<T>() where T : Window, new()
+        {
+            var popup = new T();
+            popupList.Add(popup);
+
+            return popup;
         }
 
         // Main menu
