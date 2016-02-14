029630d6eb32d5d153f01900fd693c41e2de1448
ery <ldh668@gmail.com>
2016-02-14 13:00:32 +0900

    WCF 메소드중 구현되지 않은 메소드를 구현중임.

--- Server/Sub/Lib/Common/LocalDB/LocalDB.cs
+++ Server/Sub/Lib/Common/LocalDB/LocalDB.cs
@@ -21,6 +21,8 @@ namespace IMS.Server.Sub.Lib.LocalDB
             modelBuilder.Configurations.Add(new EventLogConfiguration());
             modelBuilder.Configurations.Add(new GeneralLogConfiguration());
             modelBuilder.Configurations.Add(new UserConfiguration());
+            modelBuilder.Configurations.Add(new GroupConfiguration());
+            modelBuilder.Configurations.Add(new DevicesConfiguration());
         }
 
         public DbSet<Setting> Setting { get; set; }
@@ -28,5 +30,7 @@ namespace IMS.Server.Sub.Lib.LocalDB
         public DbSet<EventLog> EventLog { get; set; }
         public DbSet<GeneralLog> GeneralLog { get; set; }
         public DbSet<User> User { get; set; }
+        public DbSet<Group> Group { get; set; }
+        public DbSet<Devices> Devices { get; set; }
     }
 }
--- Server/Sub/WCFHost/Abstract/IIMS.cs
+++ Server/Sub/WCFHost/Abstract/IIMS.cs
@@ -7,9 +7,13 @@ namespace IMS.Server.Sub.WCFHost.Abstract
     [ServiceContract(Namespace = "http://aspt.com/IMS/", SessionMode = SessionMode.Required)]
     public interface IIMS
     {
+        // 최초 프로그램을 킬때, 사용자의 로그인을 받는다.
+        // MacAddress 를 기록하는게 필요가 있나 고민중.
         [OperationContract]
         string Athenticate(string id, string passwd, string macAddress);
 
+        // 프로그램을 종료할때, 해당 클라이언트가 종료한다는것을 알려준다.
+        // 안해도 치명적인 문제는 없지만 있는편이 좋다.
         [OperationContract]
         bool Leave();
 
@@ -20,7 +24,13 @@ namespace IMS.Server.Sub.WCFHost.Abstract
         List<IMSWarning> GetWarnings();
 
         [OperationContract]
-        IMSDeviceStatus GetDeviceStatus(int deviceId);
+        List<IMSGroup> GetGroups();
+
+        [OperationContract]
+        List<IMSDevice> GetDevicesOfSomeGroup(int groupIdx);
+
+        [OperationContract]
+        IMSDeviceStatus GetDeviceStatus(int deviceIdx);
 
         [OperationContract]
         List<IMSDeviceStatus> GetAllDeviceStatus();
