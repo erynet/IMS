4b21c2309cfc494b4f84ab90eaded5c20e347968
ery <ldh668@gmail.com>
2016-02-20 20:15:04 +0900

    LocalDBDriver 의 함수 틀을 세움.

--- Client/IMS.Client.Core/Data/DB/LocalDBDriver.cs
+++ Client/IMS.Client.Core/Data/DB/LocalDBDriver.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using IMS.Database.LocalDB;
 
 namespace IMS.Client.Core.Data.DB
 {
     public class LocalDBDriver
     {
+        #region Group
         public static List<Group.Info> GetGroups(bool ascending = true)
         {
             try
@@ -17,11 +19,11 @@ namespace IMS.Client.Core.Data.DB
                         return (from g in ctx.Group
                                 orderby g.No ascending
                                 select new Group.Info()
-                                { isUsing = g.Enabled, }).ToList();
+                                { }).ToList();
                     return (from g in ctx.Group
-                        orderby g.No descending 
-                        select new Group.Info()
-                        { isUsing = g.Enabled, }).ToList();
+                            orderby g.No descending
+                            select new Group.Info()
+                            { }).ToList();
                 }
             }
             catch (Exception e)
@@ -31,6 +33,107 @@ namespace IMS.Client.Core.Data.DB
             }
         }
 
+        public static Group.Info GetGroupByIdx(int groupIdx)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    return (from g in ctx.Group
+                            where g.Idx == groupIdx
+                            select new Group.Info()
+                            { }).DefaultIfEmpty(null).First();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetGroupByIdx : {e.ToString()}");
+                return null;
+            }
+        }
+
+        public static Group.Info GetGroupByNo(int groupNo)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    return (from g in ctx.Group
+                            where g.No == groupNo
+                            select new Group.Info()
+                            { }).DefaultIfEmpty(null).First();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetGroupByNo : {e.ToString()}");
+                return null;
+            }
+        }
+
+        public static bool SetGroup(Group.Info group)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"SetGroup : {e.ToString()}");
+                return false;
+            }
+        }
+
+        public static bool AddGroup(Group.Info group)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"AddGroup : {e.ToString()}");
+                return false;
+            }
+        }
+
+        public static bool DelGroup(int groupIdx)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"DelGroup : {e.ToString()}");
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Ups
         public static List<Ups.Info> GetUpss(bool ascending = true)
         {
             try
@@ -42,7 +145,7 @@ namespace IMS.Client.Core.Data.DB
                                 orderby u.No ascending
                                 select new Ups.Info()).ToList();
                     return (from u in ctx.Ups
-                            orderby u.No descending 
+                            orderby u.No descending
                             select new Ups.Info()).ToList();
                 }
             }
@@ -53,6 +156,107 @@ namespace IMS.Client.Core.Data.DB
             }
         }
 
+        public static Ups.Info GetUpsByIdx(int upsIdx)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    return (from u in ctx.Ups
+                            where u.Idx == upsIdx
+                            select new Ups.Info()
+                            { }).DefaultIfEmpty(null).First();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetUpsByIdx : {e.ToString()}");
+                return null;
+            }
+        }
+
+        public static Ups.Info GetUpsByNo(int upsNo)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    return (from u in ctx.Ups
+                            where u.No == upsNo
+                            select new Ups.Info()
+                            { }).DefaultIfEmpty(null).First();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetUpsByNo : {e.ToString()}");
+                return null;
+            }
+        }
+
+        public static bool SetUps(Ups.Info ups)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"SetUps : {e.ToString()}");
+                return false;
+            }
+        }
+
+        public static bool AddUps(Ups.Info ups)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"AddUps : {e.ToString()}");
+                return false;
+            }
+        }
+
+        public static bool DelUps(int upsIdx)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"DelUps : {e.ToString()}");
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Cdu
         public static List<Cdu.Info> GetCdus(bool ascending = true)
         {
             try
@@ -75,6 +279,104 @@ namespace IMS.Client.Core.Data.DB
             }
         }
 
+        public static Cdu.Info GetCduByIdx(int cduIdx)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    return (from c in ctx.Cdu
+                            where c.Idx == cduIdx
+                            select new Cdu.Info()
+                            { }).DefaultIfEmpty(null).First();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetCduByIdx : {e.ToString()}");
+                return null;
+            }
+        }
 
+        public static Cdu.Info GetCduByNo(int cduNo)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    return (from c in ctx.Cdu
+                            where c.No == cduNo
+                            select new Cdu.Info()
+                            { }).DefaultIfEmpty(null).First();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetCduByNo : {e.ToString()}");
+                return null;
+            }
+        }
+
+        public static bool SetCdu(Cdu.Info cdu)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"SetCdu : {e.ToString()}");
+                return false;
+            }
+        }
+
+        public static bool AddCdu(Cdu.Info cdu)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"AddCdu : {e.ToString()}");
+                return false;
+            }
+        }
+
+        public static bool DelCdu(int cduIdx)
+        {
+            try
+            {
+                using (var ctx = new LocalDB())
+                {
+                    using (var trx = new TransactionScope())
+                    {
+
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"DelCdu : {e.ToString()}");
+                return false;
+            }
+        }
+
+        #endregion
     }
 }
