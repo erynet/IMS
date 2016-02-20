a668b64f97b25e5c7e54224c3f24ec419ccdb5b6
Seung Ho Jang <shjang1992@gmail.com>
2016-02-21 00:16:50 +0900

    콘솔 디버깅 붙임

--- Client/IMS.Client.WPF/App/App.xaml.cs
+++ Client/IMS.Client.WPF/App/App.xaml.cs
@@ -5,11 +5,24 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Runtime.InteropServices;
 
 namespace IMS.Client.WPF {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        [DllImport("Kernel32.dll")]
+        public static extern bool AttachConsole(int processId);
+
+        public App()
+        {
+            Startup += OnStartup;
+        }
+
+        protected void OnStartup(object sender, StartupEventArgs e)
+        {
+            AttachConsole(-1);
+        }
     }
 }
