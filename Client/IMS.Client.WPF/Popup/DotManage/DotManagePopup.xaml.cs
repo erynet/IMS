303592a799c7889d99b7f79bf75db66f0ab81605
Seung Ho Jang <shjang1992@gmail.com>
2016-02-21 03:08:19 +0900

    접저 관리 기능 클라이언트 측 구현

--- Client/IMS.Client.WPF/Popup/DotManage/DotManagePopup.xaml.cs
+++ Client/IMS.Client.WPF/Popup/DotManage/DotManagePopup.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,34 @@ namespace IMS.Client.WPF {
     /// Interaction logic for DotManagePopup.xaml
     /// </summary>
     public partial class DotManagePopup : Window {
+        private class Data {
+            public CheckBox enabled;
+            public TextBox name;
+        }
+
+        private int cduIdx;
+        private Dictionary<int, Data> dataList = new Dictionary<int, Data>();
+
         public DotManagePopup()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 48; ++i) {
+                // _commandCollection is an instance, private member
+                BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+
+                // Retrieve a FieldInfo instance corresponding to the field
+                FieldInfo enabledField = GetType().GetField("enable" + (i + 1), flags);
+                FieldInfo nameField = GetType().GetField("name" + (i + 1), flags);
+
+                var data = new Data {
+                    enabled = enabledField.GetValue(this) as CheckBox,
+                    name = nameField.GetValue(this) as TextBox
+                };
+
+                dataList.Add(i + 1, data);
+            }
+
         }
 
         public void Refresh(int panelID)
@@ -31,13 +57,45 @@ namespace IMS.Client.WPF {
                 return;
             }
 
+            cduIdx = panelID;
 
-            if(panel.isExtended == true) {
+            var socketList = DataManager.inst.GetSocketData(panelID);
+            foreach (var socket in socketList) {
+                var id = socket.cduSocketNo;
+                var name = socket.cduSocketName;
+                var enabled = socket.isUsing;
+
+                dataList[id].enabled.IsChecked = enabled;
+                dataList[id].name.Text = name;
+            }
+
+            if (panel.isExtended == true) {
                 ExtendedDotList.Visibility = Visibility.Visible;
             }
             else {
                 ExtendedDotList.Visibility = Visibility.Hidden;
             }
+        }
+
+        private void button_save_Click(object sender, RoutedEventArgs e)
+        {
+            var list = new List<CduSocket.Info>();
+
+            foreach (var pair in dataList) {
+                if (pair.Value.name.Text == "") {
+                    continue;
+                }
+
+                var socketInfo = new CduSocket.Info();
+                socketInfo.cduIdx = cduIdx;
+                socketInfo.isUsing = pair.Value.enabled.IsChecked ?? false;
+                socketInfo.cduSocketName = pair.Value.name.Text;
+                socketInfo.cduSocketNo = pair.Key;
+
+                list.Add(socketInfo);
+            }
+
+            DataManager.inst.EditSocket(cduIdx, list);
         }
     }
 }
